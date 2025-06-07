2ec1e10af746ee05b77819e95d613a1d3e082bd6
Ayush-alt-sys <igayushacharya@gmail.com>
2025-06-07 17:13:10 +0530

    Start work on react and tailwind

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM, { createRoot } from 'react-dom/client'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('all-projects'));
+root.render(
+  <App />
+)
